4108fa3d2246cbf7fac4920efd7c0d0f7414d754
Shuxian Chen <114314097+Samuel-D23@users.noreply.github.com>
2023-12-10 16:34:13 -0500

    upload plots

--- make_plot.py
+++ make_plot.py
@@ -11,12 +11,10 @@ df2 = pd.read_csv('/Users/samuelchen/Desktop/UMICH/STATS 406/stats406_project/ci
 
 # 合并两个数据集
 df = pd.concat([df1, df2])
+print(df)
 
 # 将日期从字符串转换为日期对象
 df['date'] = pd.to_datetime(df['date'], format='%Y%m%d')
-
-# 添加一个通用年份列，以便对齐数据
-df['common_date'] = df['date'].apply(lambda x: x.replace(year=2020))
 
 # 以城市分组
 cities = df.groupby('city')
@@ -24,16 +22,21 @@ cities = df.groupby('city')
 # 为每个城市绘图
 for city, data in cities:
     # 排序数据
-    data = data.sort_values('common_date')
+    data = data.sort_values('date')
     
     # 筛选数据
-    data_2018_2019 = data[(data['date'] >= pd.to_datetime('2018-12-01')) & (data['date'] <= pd.to_datetime('2019-06-30'))]
-    data_2019_2020 = data[(data['date'] >= pd.to_datetime('2019-12-01')) & (data['date'] <= pd.to_datetime('2020-06-30'))]
-
+    data_2018_2019 = data[(data['date'] >= '2018-12-01') & (data['date'] <= '2019-06-30')]
+    data_2019_2020 = data[(data['date'] >= '2019-12-01') & (data['date'] <= '2020-06-30')]
+    data_2019_2020['date'] = data_2019_2020['date'] - pd.DateOffset(years=1)
+    
     # 绘制图形
     plt.figure(figsize=(10,5))
     plt.plot(data_2018_2019['date'], data_2018_2019['AQI_avg'], linestyle='--', label='2018-12 to 2019-06')
     plt.plot(data_2019_2020['date'], data_2019_2020['AQI_avg'], linestyle='-', label='2019-12 to 2020-06')
+    
+    plt.title(f'AQI Time Series for {city}')
+    plt.xlabel('Date')
+    plt.ylabel('AQI Value')
     
     # 设置横轴格式，仅显示月和日
     plt.gca().xaxis.set_major_formatter(mdates.DateFormatter('%m-%d'))
