8af51aabe249dec0aebdf910889d99dcd1a412e0
Shuxian Chen <114314097+Samuel-D23@users.noreply.github.com>
2023-12-12 16:45:24 -0500

    upload true data

--- simulation data/data_cleaning_for_simulation.py
+++ simulation data/data_cleaning_for_simulation.py
@@ -2,7 +2,7 @@ import pandas as pd
 import os
 
 # Load the Excel file
-file_path = '/Users/samuelchen/Desktop/UMICH/STATS 406/stats406_project/city daily/combined_china_cities_2019.csv'
+file_path = '/Users/samuelchen/Desktop/UMICH/STATS 406/stats406_project/city daily/combined_china_cities_2020.csv'
 data = pd.read_csv(file_path)
 
 # Get a list of unique cities
@@ -20,13 +20,13 @@ def filter_data_by_date_range(df, start_date, end_date):
     return df[(df['date'] >= start_date) & (df['date'] <= end_date)]
 
 # Output folder for the Excel files
-output_folder = '/Users/samuelchen/Desktop/UMICH/STATS 406/stats406_project/simulation test'
+output_folder = '/Users/samuelchen/Desktop/UMICH/STATS 406/stats406_project/simulation data/true'
 os.makedirs(output_folder, exist_ok=True)
 
 # Process data for each city and save to separate Excel files
 for city in cities:
     city_data = data[data['city'] == city]
-    output_file_path = os.path.join(output_folder, f'{city}_data_2019.xlsx')
+    output_file_path = os.path.join(output_folder, f'{city}_data_2020.xlsx')
     
     with pd.ExcelWriter(output_file_path, engine='xlsxwriter') as writer:
         for sheet, date_range in date_ranges.items():
